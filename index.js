e4dd07723ceb1bf23ef67a185165a55a7388c24d
elinalivaprince <liiva.prince@gmail.com>
2025-05-12 20:08:22 +0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,47 @@
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeScoreEl = document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+
+
+function increaseHomeScoreThree(){
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+
+let homeScoreBtn2One = document.getElementById("home-score-btn-2-1")
+let homeScore2El = document.getElementById("home-score-2")
+let home2Score = 0
+
+function increaseHomeScore2One(){
+    home2Score += 1
+    homeScore2El.textContent = home2Score
+}
+
+let homeScoreBtn2Two = document.getElementById("home-score-btn-2-2")
+
+function increaseHomeScore2Two(){
+    home2Score += 2
+    homeScore2El.textContent = home2Score
+}
+
+let homeScoreBtn2Three = document.getElementById("home-score-btn-2-3")
+
+
+function increaseHomeScore2Three(){
+    home2Score += 3
+    homeScore2El.textContent = home2Score
+}
